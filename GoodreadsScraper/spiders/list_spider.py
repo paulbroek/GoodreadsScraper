87fa0c4316097f2b51053bde552428fca35f844f
paulbroek <pauliusbroek@hotmail.com>
2021-12-06 13:50:09 +0100

    scrape books using redis input

--- GoodreadsScraper/spiders/list_spider.py
+++ GoodreadsScraper/spiders/list_spider.py
@@ -15,9 +15,10 @@ class ListSpider(scrapy.Spider):
 
     goodreads_list_url = "https://www.goodreads.com/list/show/{}?page={}"
 
-    def __init__(self, list_name, start_page_no, end_page_no):
+    def __init__(self, list_name, start_page_no, end_page_no, books_from_redis=False):
         super().__init__()
         self.book_spider = BookSpider()
+        self.books_from_redis = books_from_redis
 
         self.start_urls = []
         for page_no in range(int(start_page_no), int(end_page_no) + 1):
@@ -28,7 +29,8 @@ class ListSpider(scrapy.Spider):
         list_of_books = response.css("a.bookTitle::attr(href)").extract()
 
         # manual input override. don't know other way for now
-        list_of_books = ["/book/show/16085481-crazy-rich-asians", "/book/show/199519.The_Tibetan_Yogas_Of_Dream_And_Sleep"]
+        list_of_books = ["/book/show/16085481-crazy-rich-asians", "/book/show/199519.The_Tibetan_Yogas_Of_Dream_And_Sleep", "/book/show/50512348-the-message-game"]
+        # or listen for books through redis
         # print(f'{list_of_books=}')
         print(f'{len(list_of_books)=}')
 
