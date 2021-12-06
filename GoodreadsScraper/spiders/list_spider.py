753ffc8a8289ecbd2932d56ed36e8baaef75c829
paulbroek <pauliusbroek@hotmail.com>
2021-12-06 14:37:48 +0100

    generator can be exhausted

--- GoodreadsScraper/spiders/list_spider.py
+++ GoodreadsScraper/spiders/list_spider.py
@@ -57,9 +57,10 @@ class ListSpider(scrapy.Spider):
         list_of_books = response.css("a.bookTitle::attr(href)").extract()
 
         # manual input override. don't know other way for now
-        list_of_books = ["/book/show/16085481-crazy-rich-asians", "/book/show/199519.The_Tibetan_Yogas_Of_Dream_And_Sleep", "/book/show/50512348-the-message-game"]
+        # list_of_books = ["/book/show/16085481-crazy-rich-asians", "/book/show/199519.The_Tibetan_Yogas_Of_Dream_And_Sleep", "/book/show/50512348-the-message-game"]
         # or listen for books through redis
         # list_of_books = self.redis_generator()
+        list_of_books = list(self.redis_generator())
         # print(f'{list_of_books=}')
         if isinstance(list_of_books, list):
             print(f'{len(list_of_books)=}')
