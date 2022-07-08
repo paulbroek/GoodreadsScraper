9d27b33e25a727296f55265da5c1fe880214c425
paulbroek <pauliusbroek@hotmail.com>
2022-07-08 11:15:24 +0200

    add author-list scraper

---
+++ GoodreadsScraper/spiders/author_list_spider.py
@@ -0,0 +1,41 @@
+"""Spider to extract URL's of books from a Listopia list on Goodreads"""
+
+import logging
+
+import scrapy
+
+from .book_spider import BookSpider
+
+GOODREADS_URL_PREFIX = "https://www.goodreads.com"
+
+logger = logging.getLogger(__name__)
+
+class AuthorListSpider(scrapy.Spider):
+    """Extract URLs of books from a list of books by author
+
+    example list: https://www.goodreads.com/author/list/4562806.Dan_S_Kennedy
+
+        This subsequently passes on the URLs to BookSpider
+    """
+    name = "author-list"
+
+    goodreads_list_url = "https://www.goodreads.com/author/list/{}?page={}"
+
+    def __init__(self, list_name, start_page_no, end_page_no):
+        super().__init__()
+        self.book_spider = BookSpider()
+
+        self.start_urls = []
+        for page_no in range(int(start_page_no), int(end_page_no) + 1):
+            list_url = self.goodreads_list_url.format(list_name, page_no)
+            self.start_urls.append(list_url)
+
+    def parse(self, response):
+        list_of_books = response.css("a.bookTitle::attr(href)").extract()
+
+        if isinstance(list_of_books, list):
+            logger.info(f'{len(list_of_books)=}')
+
+        for book in list_of_books:
+            logger.info(f"yielding {book=}")
+            yield response.follow(book, callback=self.book_spider.parse)
